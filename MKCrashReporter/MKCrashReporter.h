81a164246a318357a4d4ebffcc7e3542f2a072d7
Manju Kiran <email@manjukiran.com>
2015-02-20 14:35:14 +0000

    Initial Commit

---
+++ MKCrashReporter/MKCrashReporter.h
@@ -0,0 +1,19 @@
+//
+//  MKCrashReporter.h
+//  MKCrashReporter
+//
+//  Created by Manju Kiran on 20/02/2015.
+//  Copyright (c) 2015 Manju Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKCrashReporter.
+FOUNDATION_EXPORT double MKCrashReporterVersionNumber;
+
+//! Project version string for MKCrashReporter.
+FOUNDATION_EXPORT const unsigned char MKCrashReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKCrashReporter/PublicHeader.h>
+
+
